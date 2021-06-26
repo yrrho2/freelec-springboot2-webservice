9e61922136b3bceabcc1ba49d4900c505f3cc68c
Dong young <yrrho2@naver.com>
2021-06-26 23:28:27 +0900

    Commit 76p ""

--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,9 +1,12 @@
 package com.jojoldu.book.springboot.web.dto;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+@Getter
+@RequiredArgsConstructor
 public class HelloResponseDtoTest {
     @Test
     public void 롬복_기능_테스트(){
