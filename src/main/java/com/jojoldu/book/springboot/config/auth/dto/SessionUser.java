29af9ad4af43daecf14b3d133fb74f4287dfcf34
Dong young <yrrho2@naver.com>
2021-08-06 03:05:36 +0900

    Commit 211p "Start at 5.7"

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/SessionUser.java
@@ -4,8 +4,10 @@ package com.jojoldu.book.springboot.config.auth.dto;
 import com.jojoldu.book.springboot.comain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
--- src/main/java/com/jojoldu/book/springboot/web/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/OAuthAttributes.java
@@ -31,6 +31,11 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId
                                     , String userNameAttributeName
                                     , Map<String, Object> attributes) {
+
+        if ("naver".equals(registrationId)) {
+            return ofNaver("Id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
      private static OAuthAttributes ofGoogle(String userNameAttributeName
@@ -52,4 +57,17 @@ public class OAuthAttributes {
                 .role(Role.GUEST)
                 .build();
      }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+
+    }
+
 }
